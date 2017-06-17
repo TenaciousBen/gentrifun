f600688d1bbdaf51ad52a3843599172c236a9596
TenaciousBen <bhajduk@gmail.com>
2017-06-17 20:16:15 +0100

    added ts dependencies and config into react app

---
+++ FE/src/config.ts
@@ -0,0 +1,19 @@
+export interface IConfig {
+    apiBase: string;
+    contentBase: string;
+}
+
+//matches a given frontend host to the config for that host, so that the api base will
+//automatically change depending on the FE host
+function getConfig(): IConfig {
+    var currentHost = window.location.hostname;
+    switch (currentHost) {
+        case "localhost": return { apiBase: "localhost:9000", contentBase: currentHost };
+        //add additional hosts here e.g. prod, test etc
+        default: throw new Error(`Unrecognized content host '${currentHost}'`)
+    }
+};
+
+const config = getConfig();
+
+export default config;
